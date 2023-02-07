dc612e48eae1ceff266e61a69f3feec8a803fe7b
breadlofe <breadlofe@gmail.com>
2023-02-07 13:23:24 -0500

    removed extra build folder creation issue.

--- Build/cmake_install.cmake
+++ Build/cmake_install.cmake
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/breadlofe/Desktop/Data_Structures/ssuds_pp/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/breadlofe/Desktop/Data_Structures/ssuds_pp/Build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "arraylist.h"
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, world!\n";
+    std::cout << "Hello, wold!\n";
 
 ::testing::InitGoogleTest(&argc, argv);
 return RUN_ALL_TESTS();
