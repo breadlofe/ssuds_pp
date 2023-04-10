49e22e1bdaff6e060267d3b34808dd756028a86f
breadlofe <breadlofe@gmail.com>
2023-04-10 13:23:22 -0400

    Finished GTEST for add, contains, height, size

--- src/settest.cpp
+++ src/settest.cpp
@@ -20,4 +20,37 @@ TEST_F(SetTestFixture, Size1)
     EXPECT_EQ(test_ss.size(), 0);
 }
 
+TEST_F(SetTestFixture, Add1)
+{
+    EXPECT_TRUE(test_si.add(1));
+}
+
+TEST_F(SetTestFixture, Add2)
+{
+    EXPECT_TRUE(test_ss.add("Bob"));
+    EXPECT_TRUE(test_ss.add("John"));
+    EXPECT_EQ(test_ss.size(), 2);
+}
+
+TEST_F(SetTestFixture, Contains1)
+{
+    test_ss.add("Bob");
+    test_ss.add("John");
+    EXPECT_TRUE(test_ss.contains("Bob"));
+}
+
+TEST_F(SetTestFixture, Contains2)
+{
+    test_ss.add("Bob");
+    test_ss.add("John");
+    EXPECT_FALSE(test_ss.contains("Bill"));
+}
+
+TEST_F(SetTestFixture, Height1)
+{
+    test_ss.add("Bob");
+    test_ss.add("John");
+    EXPECT_EQ(test_ss.get_height(), 2);
+}
+
 #endif
