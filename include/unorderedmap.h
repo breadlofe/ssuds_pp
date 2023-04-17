a5dc5c3b68e2580f75b84858005a8a14bc2728e4
breadlofe <breadlofe@gmail.com>
2023-04-17 10:09:47 -0400

    Can add through bracket operator

--- include/unorderedmap.h
+++ include/unorderedmap.h
@@ -56,41 +56,46 @@ namespace ssuds
         /// @return Bool: False if key found, true if not.
         bool in_table(unsigned int* index, K the_key)
         {
-            if(mTable[*index]->mKey != the_key)
+            // Understand that, because of the hashed index, we begin where the
+            // value should be originaly placed. By checking if it is NULL first, 
+            // we are saying that the slot we originally wanted to put it is empty,
+            // thus it is not in the table.
+            if(mTable[*index] == NULL)
+                return false;
+            else
             {
-                if(*index < mSize)
+                if(mTable[*index]->mKey != the_key)
                 {
                     (*index)++;
-                    // question: will I have to check for wrap around cases?
+                    if((*index) == mCapacity)
+                        (*index) = 0;
                     return in_table(index, the_key);
                 }
                 else
-                    return false;
+                    return true;
             }
-            else
-                return true;
         }
 
         /// @brief Helper function for bracket operator that increases capicity if 
         ///         number of used slots exceeds 70% of the previous capacity.
         /// @return An array of node*
-        node** grow_helper()
+        void grow_helper()
         {
+            node** temp = mTable;
             mCapacity *= 2u;
-            node** temp = new node*[mCapacity];
-                //             for(int i=0; i<mSize; i++)
-                // {
-                //     temp[i] = mData[i];
-                // }
-                // delete[] mData;
-                // mData = temp;
-            for(int i=0; i<mSize; i++)
+            mTable = new node*[mCapacity];
+            mSize = 0;      // Needed since we will be re-adding values.
+            for(int i = 0; i < mCapacity; i++)
             {
-                temp[i]->mValue = mTable[i]->mValue;
-                //V& temp_val = this[temp[i]->mKey];
-                temp[i]->mValue = (*this)[temp[i]->mKey];
+                mTable[i] = nullptr;
             }
-            return temp;
+            for(int i = 0; i < (int)(mCapacity/2u); i++)
+            {
+                if(temp[i] != NULL)
+                {
+                    (*this)[temp[i]->mKey] = temp[i]->mValue;
+                }
+            }
         }
 
     public:
@@ -101,6 +106,10 @@ namespace ssuds
             mTable = new node*[mMin_Capacity];
             mSize = 0;
             mCapacity = mMin_Capacity;
+            for(int i = 0; i < mMin_Capacity; i++)
+            {
+                mTable[i] = nullptr;
+            }
         }
 
         /// @brief Default destructor for UnorderedMap which deletes mTable as an array.
@@ -115,16 +124,14 @@ namespace ssuds
         /// @return Returns reference to hash value.
         V& operator[](K the_key)
         {
-            //probably put grow here
-            //find the index up here.
             std::hash<K> key_hash;
+            if(mSize >= (int)(mCapacity*0.7f))
+            {
+                grow_helper();
+            }
             unsigned int index = key_hash(the_key) % mCapacity;
             if(!in_table(&index, the_key))
             {
-                if(mSize >= (int)(mCapacity*0.7f))
-                {
-                    mTable = grow_helper();
-                }
                 node* new_pair = new node(the_key);
                 mTable[index] = new_pair;
                 mSize++;
--- src/maptest.cpp
+++ src/maptest.cpp
@@ -9,16 +9,20 @@ class MapTestFixture : public::testing::Test
 {
 protected:
     void SetUp() override {
+        test_map["Alice"] = 1;
     }
-
+    ssuds::UnorderedMap<std::string,int> test_map;
 };
 
 TEST_F(MapTestFixture, BracketOP1)
 {
-    ssuds::UnorderedMap<std::string,int> test_map;
-    test_map["Bob"] = 2;
+    ssuds::UnorderedMap<std::string,int> test_map_here;
+    test_map_here["Bob"] = 2;
+}
 
-    //test_map[1] = 1;
+TEST_F(MapTestFixture, BracktOP2)
+{
+    EXPECT_EQ(test_map["Alice"], 1);
 }
 
 #endif
