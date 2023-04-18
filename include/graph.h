deed1c80c6f14cc472e1ccbdd5ae58440bfc7f63
breadlofe <breadlofe@gmail.com>
2023-04-18 13:01:21 -0400

    class skeleton

--- include/graph.h
+++ include/graph.h
@@ -6,6 +6,30 @@ namespace ssuds
     class Graph
     {
     protected:
+        std::map<N, std::map<N,E>> the_graph;
+    public:
+        Graph() {}
+        ~Graph() {}
+        void add_node(const N& node_val)
+        {
+            //stuff
+        }
+        void add_edge(const E& edge_val, const N& start_node, const N& end_node)
+        {
+            //stuff
+        }
+        void remove_node(const N& node_val)
+        {
+            //stuff
+        }
+        void remove_edge(const N& start_node, const N& end_node)
+        {
+            //stuff
+        }
+        bool has_edge(const N& start_node, const N& end_node)
+        {
+            //stuff
+        }
         
     };
 }
